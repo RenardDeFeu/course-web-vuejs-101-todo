74a12232a74fce5c5d5575d45621b2400dd9dae9
RenardDeFeu <david.borg@le-campus-numerique.fr>
2022-10-28 16:11:25 +0200

    Jusqu'a crossed ça maaaaaaaaaaaAAAAAAArchEEEEEE

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//A toi de jouer ! https://youtu.be/1dLcnxIock0
+const vm = Vue.createApp({
+  data() {
+    return {
+      idCounter: 4,
+      newTask: {id:4, content:'',crossed: false},
+      todos: [
+        {id: 0, content: 'Boire du café',crossed: false},
+        {id: 1, content: 'Manger un sandwitch',crossed: false},
+        {id: 2, content: 'Apprendre vue js',crossed: false},
+        {id: 3, content: 'Faire une todo liste',crossed: false}
+      ]
+    }
+  },
+  methods:{
+    cross(indexElement){
+      this.todos[indexElement].crossed = true;
+      console.log(this.todos[indexElement].crossed);
+    },
+    addTask(){
+      this.todos.push(this.newTask);
+      this.newTask.idCounter++;
+      this.newTask = {id: this.idCounter, content:''}
+    }
+  }
+})
+
+vm.mount('#app')
