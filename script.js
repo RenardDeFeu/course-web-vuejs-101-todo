6f0b16a0528b681015f11bf7ebbc33566e7f1598
RenardDeFeu <david.borg@le-campus-numerique.fr>
2022-11-03 11:52:54 +0100

    Exo fini

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const vm = Vue.createApp({
   data() {
     return {
-      idCounter: 4,
-      newTask: {id:4, content:'',crossed: false},
+      priorityOrder: {"1":"Normal", "2":"Important", "3":"Urgent"}, // Utilisé dans le html pour afficher les priorités
+      newTask: {content:'',crossed: false, priority:"1"},
       todos: [
-        {id: 0, content: 'Boire du café',crossed: false},
-        {id: 1, content: 'Manger un sandwitch',crossed: false},
-        {id: 2, content: 'Apprendre vue js',crossed: false},
-        {id: 3, content: 'Faire une todo liste',crossed: false}
+        {content: 'Boire du café',crossed: false, priority:"1"},
+        {content: 'Manger un sandwitch',crossed: false, priority:"1"},
+        {content: 'Apprendre vue js',crossed: false, priority:"1"},
+        {content: 'Faire une todo liste',crossed: false, priority:"1"}
       ]
     }
   },
@@ -18,9 +18,29 @@ const vm = Vue.createApp({
     },
     addTask(){
       this.todos.push(this.newTask);
-      this.newTask.idCounter++;
-      this.newTask = {id: this.idCounter, content:''}
+      this.newTask = {content:'',crossed: false, priority:"1"};
+    },
+    clearTasks(){
+      this.todos = [];
+    },
+    sortTasks(){
+      this.todos.sort(function(a, b){
+        if (a.priority < b.priority){
+          return 1;
+        }
+        if (a.priority > b.priority){
+          return -1;
+        }
+      })
     }
+  },
+  // Charge la liste dans le local storage avant le montage de l'app (hook)
+  mounted(){
+    if (localStorage.getItem("todos")) this.todos = JSON.parse(localStorage.getItem("todos"));
+  },
+  // Sauvegarde la liste dans le local storage à chaque update (hook)
+  updated(){
+    localStorage.setItem("todos", JSON.stringify(this.todos));
   }
 })
 
